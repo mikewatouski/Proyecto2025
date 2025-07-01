0c2001f22cf5092d3980b68cc7c62870ff5b6204
MatiasNiebieski <163850963+MatiasNiebieski@users.noreply.github.com>
2025-07-01 10:40:36 -0300

    index

---
+++ home.js
@@ -0,0 +1,12 @@
+function irAFormulario() {
+    window.location.href = "formulario.html";
+  }
+  
+  function irARegistro() {
+    alert("La sección de registro estará disponible pronto.");
+  }
+  
+  function irALogin() {
+    alert("La sección de inicio de sesión estará disponible pronto.");
+  }
+  

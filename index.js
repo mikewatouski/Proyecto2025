a252f6c90643a78d4af62f6fda73f1f6e43cf922
MatiasNiebieski <163850963+MatiasNiebieski@users.noreply.github.com>
2025-07-01 10:03:49 -0300

    index

---
+++ index.js
@@ -0,0 +1,20 @@
+document.getElementById('formulario-deporte').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const deporte = document.getElementById('deporte').value;
+    const intensidad = document.getElementById('intensidad').value;
+  
+    if (!deporte || !intensidad) {
+      document.getElementById('resultado').textContent = 'Por favor, completá todos los campos.';
+      return;
+    }
+  
+    
+    const params = new URLSearchParams({
+      deporte: deporte,
+      intensidad: intensidad
+    });
+  
+    window.location.href = `index.html?${params.toString()}`;
+  });
+  
